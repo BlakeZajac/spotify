ffc7b95a8aad9667f95eef3d67af1637680205a6
Blake Zajac <contact@blakezajac.com>
2023-08-03 22:16:06 +1000

    Set up Auth with GitHub and Spotify. Implement toast messages for success/error

--- components/auth-modal.tsx
+++ components/auth-modal.tsx
@@ -6,8 +6,11 @@ import {
 } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/navigation";
 import { Auth } from "@supabase/auth-ui-react";
+import useAuthModal from "@/hooks/useAuthModal";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 import Modal from "@/components/modal";
+import { useEffect } from "react";
 
 interface AuthModalProps {
   children: React.ReactNode;
@@ -17,15 +20,45 @@ const AuthModal: React.FC<AuthModalProps> = ({ children }) => {
   const supabaseClient = useSupabaseClient();
   const router = useRouter();
   const { session } = useSessionContext();
+  const { onClose, isOpen } = useAuthModal();
+
+  useEffect(() => {
+    if (session) {
+      router.refresh();
+      onClose();
+    }
+  }, [session, router, onClose]);
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
 
   return (
     <Modal
       title="Welcome back!"
       description="Login to your account"
-      isOpen
-      onChange={() => {}}
+      isOpen={isOpen}
+      onChange={onChange}
     >
-      <Auth supabaseClient={supabaseClient} />
+      <Auth
+        theme="dark"
+        magicLink
+        providers={["github", "spotify"]}
+        supabaseClient={supabaseClient}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: "#2e2e2e",
+                brandAccent: "#3e3e3e",
+              },
+            },
+          },
+        }}
+      />
     </Modal>
   );
 };
--- components/header.tsx
+++ components/header.tsx
@@ -5,8 +5,13 @@ import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
-
 import Button from "@/components/button";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
+import { toast } from "react-hot-toast";
+import { useEffect } from "react";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -14,11 +19,30 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const authModal = useAuthModal();
   const router = useRouter();
 
-  const handleLogout = () => {
-    // Handle logout
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    // Reset any playing songs
+    router.refresh();
+
+    if (error) {
+      console.log(error);
+      toast.error(error.message);
+    } else {
+      toast.success("You are now logged out.");
+    }
   };
+
+  useEffect(() => {
+    if (user) {
+      toast.success("You are now logged in.");
+    }
+  }, [user]);
 
   return (
     <div
@@ -55,22 +79,40 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         </div>
 
         <div className="flex justify-between items-center gap-x-4">
-          <>
-            <div>
-              <Button
-                onClick={() => {}}
-                className="bg-transparent text-neutral-300 font-medium"
-              >
-                Sign Up
+          {user ? (
+            <div className="flex gap-x-4 items-center">
+              <Button onClick={handleLogout} className="bg-white px-6 py-2">
+                Logout
               </Button>
-            </div>
 
-            <div>
-              <Button onClick={() => {}} className="bg-white px-6 py-2">
-                Log In
+              <Button
+                onClick={() => router.push("/account")}
+                className="bg-white"
+              >
+                <FaUserAlt />
               </Button>
             </div>
-          </>
+          ) : (
+            <>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="bg-transparent text-neutral-300 font-medium"
+                >
+                  Sign Up
+                </Button>
+              </div>
+
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="bg-white px-6 py-2"
+                >
+                  Log In
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {children}
