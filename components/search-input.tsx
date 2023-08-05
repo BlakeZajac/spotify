1f7ac0610a36c94e0548253dcffe25733bd93a7c
Blake Zajac <contact@blakezajac.com>
2023-08-05 15:16:56 +1000

    Set up search input

---
+++ components/search-input.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import qs from "query-string";
+
+interface SearchInputProps {}
+
+const SearchInput: React.FC<SearchInputProps> = ({}) => {
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  useEffect(() => {
+    const query = {
+      title: debouncedValue,
+    };
+
+    const url = qs.stringifyUrl({
+      url: "/search",
+    });
+  }, []);
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default SearchInput;
